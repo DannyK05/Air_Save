1b81c339d14c2899427549d18c72758567f384b5
DannyK05 <olubanjok@gmail.com>
2024-04-15 14:14:00 +0100

    feat: "Implemented the ui for the homepage and layout"

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,19 +1,37 @@
+import { useState } from "react"
+
 export default function Layout ({children}: any){
+    const [darkMode, setDarkMode] = useState(false)
+
+    const activateDarkMode = () =>{
+        setDarkMode(!darkMode)
+    }
     return(
-        <>
-        <header></header>
-        <main>
-        {children}
+        <div className={`${darkMode ? "text-white bg-[#11141C]" :""} w-full h-full p-4 `}>
+        <header className="flex items-center justify-between">
+            <h1>AirSave</h1>
+            <span onClick={activateDarkMode}>
+            {
+                darkMode ? 
+                <ion-icon name="moon-outline" size="large"></ion-icon>
+                :
+                <ion-icon name="sunny-outline" size="large"></ion-icon>
+            }
+            </span> 
+            
+        </header>
+        <main className="flex flex-col items-center space-y-6">
+            {children}
         </main>
         <footer>
-            <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+            <nav className="flex items-center justify-center">
+                <ul className="flex items-center space-x-6">
+                    <li><ion-icon name="home-outline" size="large"></ion-icon></li>
+                    <li><ion-icon name="radio-button-on-outline" size="large"></ion-icon></li>
+                    <li><ion-icon name="flash-outline" size="large"></ion-icon></li>
                 </ul>
             </nav>
         </footer>
-        </>
+        </div>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,10 +3,27 @@ import Layout from "../../layout";
 export default function Home (){
     return(
         <Layout>
-            <div className="border ">
+            <div className="border w-auto rounded-full lg:p-8 ">
                 <h1 className="">42</h1>
             </div>
-            <p></p>
+            <p className={`text-lg`}>Bad</p>
+            <div className="rounded-lg flex items-center space-x-3 ">
+                <p className="flex flex-col items-center">
+                    <span>Pollutant</span>
+                    <span>O3</span>
+                </p>
+                <p className="flex flex-col items-center">
+                    <span>Country</span>
+                    <span>USA</span>
+                </p>
+                <p className="flex flex-col items-center">
+                    <span>Pollutant</span>
+                    <span>O3</span>
+                </p>
+            </div>
+            <div>
+                <p>Remark</p>
+            </div>
         </Layout>
     )
 }
