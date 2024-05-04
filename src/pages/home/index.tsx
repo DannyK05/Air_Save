c521bc493c17d0c0d2895bb543a3a97ae26cd692
DannyK05 <olubanjok@gmail.com>
2024-05-04 10:46:04 +0100

    refactor: refactored the code to call the air quality api on the client side

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,148 +1,71 @@
 import Layout from "../../layout";
-import { useState } from "react";
-// const aqiResponse = {
-//     "dateTime": "2023-06-15T10:00:00Z",
-//     "regionCode": "us",
-//     "indexes": [
-//         {
-//             "code": "uaqi",
-//             "displayName": "Universal AQI",
-//             "aqi": 89,
-//             "aqiDisplay": "89",
-//             "color": {
-//                 "red": 48,
-//                 "green": 175,
-//                 "blue": 55,
-//                 "alpha": 255
-//             },
-//             "category": "Excellent air quality",
-//             "dominantPollutant": "o3"
-//         },
-//         {
-//             "code": "usa_epa",
-//             "displayName": "AQI (US)",
-//             "aqi": 41,
-//             "aqiDisplay": "41",
-//             "color": {
-//                 "green": 228,
-//                 "alpha": 255
-//             },
-//             "category": "Good air quality",
-//             "dominantPollutant": "pm25"
-//         }
-//     ],
-//     "pollutants": [
-//         {
-//             "code": "co",
-//             "displayName": "CO",
-//             "fullName": "Carbon monoxide",
-//             "concentration": {
-//                 "value": 334.24,
-//                 "units": "PARTS_PER_BILLION"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Typically originates from incomplete combustion...",
-//                 "effects": "When inhaled, carbon monoxide can prevent the blood from..."
-//             }
-//         },
-//         {
-//             "code": "no2",
-//             "displayName": "NO2",
-//             "fullName": "Nitrogen dioxide",
-//             "concentration": {
-//                 "value": 13.12,
-//                 "units": "PARTS_PER_BILLION"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Main sources are fuel burning processes, such as...",
-//                 "effects": "Exposure may cause increased bronchial reactivity..."
-//             }
-//         },
-//         {
-//             "code": "o3",
-//             "displayName": "O3",
-//             "fullName": "Ozone",
-//             "concentration": {
-//                 "value": 13.88,
-//                 "units": "PARTS_PER_BILLION"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Ozone is created in a chemical reaction between atmospheric oxygen...",
-//                 "effects": "Ozone can irritate the airways and cause coughing, a burning..."
-//             }
-//         },
-//         {
-//             "code": "pm10",
-//             "displayName": "PM10",
-//             "fullName": "Inhalable particulate matter (<10µm)",
-//             "concentration": {
-//                 "value": 5.07,
-//                 "units": "MICROGRAMS_PER_CUBIC_METER"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Main sources are combustion processes (e.g. indoor heating, wildfires)...",
-//                 "effects": "Inhalable particles can penetrate into the lungs. Short term exposure..."
-//             }
-//         },
-//         {
-//             "code": "pm25",
-//             "displayName": "PM2.5",
-//             "fullName": "Fine particulate matter (<2.5µm)",
-//             "concentration": {
-//                 "value": 2.83,
-//                 "units": "MICROGRAMS_PER_CUBIC_METER"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Main sources are combustion processes (e.g. power plants, indoor heating...",
-//                 "effects": "Fine particles can penetrate into the lungs and bloodstream. Short term..."
-//             }
-//         },
-//         {
-//             "code": "so2",
-//             "displayName": "SO2",
-//             "fullName": "Sulfur dioxide",
-//             "concentration": {
-//                 "value": 0.02,
-//                 "units": "PARTS_PER_BILLION"
-//             },
-//             "additionalInfo": {
-//                 "sources": "Main sources are burning processes of sulfur-containing fuel in industry...",
-//                 "effects": "Exposure causes irritation of the respiratory tract, coughing and generates..."
-//             }
-//         }
-//     ],
-//     "healthRecommendations": {
-//         "generalPopulation": "With this level of air quality, you have no limitations. Enjoy the outdoors!",
-//         "elderly": "If you start to feel respiratory discomfort such as coughing or breathing difficulties...",
-//         "lungDiseasePopulation": "...",
-//         "heartDiseasePopulation": "...",
-//         "athletes": "...",
-//         "pregnantWomen": "...",
-//         "children": "..."
-//     }
-// }
+import { useEffect, useState } from "react";
+
 export default function Home (){
     const [aqiError, setAqiError] = useState("")
     const[aqiData, setAqiData] = useState()
+    const [userLocation, setUserLocation] = useState({});
+    const apiKey =import.meta.env.VITE_APP_AQI_API_KEY
+
+
      const getAQIData = async (e:any) =>{
         e.preventDefault()
-        await fetch('http://localhost:3000/aqi?latitude=37.419734&longitude=-122.0827784')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            else{
-                console.log(response.json())
-                setAqiData(response)
-            }
-        })
-        .then(data => console.log(data))
-        .catch((error) => {
-            setAqiError(error)
-            console.error('Error fetching data:', error)
-
-        });
+        const url = `https://airquality.googleapis.com/v1/currentConditions:lookup?key=${apiKey}`;
+        const data = {
+          location: {
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude
+          },
+          extraComputations: [
+            "HEALTH_RECOMMENDATIONS",
+            "DOMINANT_POLLUTANT_CONCENTRATION",
+            "POLLUTANT_CONCENTRATION",
+            "LOCAL_AQI",
+            "POLLUTANT_ADDITIONAL_INFO"
+          ]
+        };
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        };
+  
+        try {
+          const response = await fetch(url, options);
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          console.log(response)
+          const json = await response.json();
+          console.log(json)
+          setAqiData(json);
+        } catch (error) {
+          setAqiError(error.message);
+          console.error("Error fetching data:", error);
+        }
+       
      }
+
+     useEffect(() => {
+        const fetchUserLocation = () => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setUserLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+              });
+            },
+            (error) => {
+              console.error("Geolocation Error:", error);
+            }
+          );
+        };
+    
+        fetchUserLocation();
+        console.log(userLocation)
+      }, []);
      return(
         <Layout action = {getAQIData}>
             {aqiData ?
@@ -177,14 +100,14 @@ export default function Home (){
             </div>
             <div>
                 <p>Remark</p>
-                <p>{aqiData.healthRecommendations.generalPopulation}</p>
+                <p>{aqiData.healthRecommendations.generalPopulation}</p> 
             </div></>
              :(<>
                 <div className="border p-6 w-auto rounded-full lg:p-8 ">
                 <h1 className="">--</h1>
             </div>
             <p className={`text-lg`}>--</p>
-            <div className="rounded-lg flex flex-col items-center space-x-3 hover:shadow">
+            <div className="rounded-lg flex flex-col items-center space-x-3 p-3 hover:shadow">
                 <div className=" w-full flex flex-col items-center p-4">
                     <p>Pollutants</p>
                     <div className="flex items-center flex-wrap">
